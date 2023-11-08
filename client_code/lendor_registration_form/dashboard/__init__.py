910cae3fbedafcf3123b3d09da8fbaac4ab5444d
sanjanagowdanar@gmail.com <sanjanagowdanar@gmail.com>
2023-11-08 08:07:37 +0000

    Created lendor_registration_form.Lender_reg_Institutional_bank_form_1, lendor_registration_form.Lender_reg_Institutional_bank_form_2, lendor_registration_form.Lender_reg_Institutional_form_1, lendor_registration_form.Lender_reg_Institutional_form_2 and 6 more; Edited lendor_registration_form.Lender_reg_individual_form_1

--- client_code/lendor_registration_form/dashboard/__init__.py
+++ client_code/lendor_registration_form/dashboard/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Lender_reg_individual_form_1Template
+from ._anvil_designer import dashboardTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,18 +8,13 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Lender_reg_individual_form_1(Lender_reg_individual_form_1Template):
+class dashboard(dashboardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
-  def button_2_click(self, **event_args):
-    open_form('lendor_registration_form.Lender_reg_individual_form_2')
+  def outlined_button_6_click(self, **event_args):
     """This method is called when the button is clicked"""
-
-  def button_1_click(self, **event_args):
-    open_form('lendor_registration_form.Lender_reg_form_8')
-    """This method is called when the button is clicked"""
-    
+    pass
