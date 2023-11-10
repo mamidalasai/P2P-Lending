76b363d031b6da1d677bb2d2b3feb45066f6e85f
saimamidala333@gmail.com <saimamidala333@gmail.com>
2023-11-10 12:24:30 +0000

    Created admin.dashboard.admin_view_profile, admin.dashboard.admin_view_profile.RowTemplate2, admin.dashboard.admin_view_profile.admin_user_profile_3, admin.dashboard.admin_view_profile.admin_user_profile_3.RowTemplate4 and 6 more

---
+++ client_code/admin/dashboard/admin_view_profile/admin_view_profile_4/__init__.py
@@ -0,0 +1,24 @@
+from ._anvil_designer import admin_view_profile_4Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class admin_view_profile_4(admin_view_profile_4Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('admin.dashboard.admin_view_profile.admin_view_profile_5')
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('admin.dashboard.admin_view_profile.admin_user_profile_3')
