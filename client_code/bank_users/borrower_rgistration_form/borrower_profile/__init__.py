60515e4e551076fe3a36f556058996a7a0b05429
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-11-16 07:47:36 +0000

    Edited bank_users.user_form.about_user_form, settings and bank_users.borrower_rgistration_form.borrower_profile

--- client_code/bank_users/borrower_rgistration_form/borrower_profile/__init__.py
+++ client_code/bank_users/borrower_rgistration_form/borrower_profile/__init__.py
@@ -14,7 +14,7 @@ class borrower_profile(borrower_profileTemplate):
     self.init_components(**properties)
     user_id=get_current_user_id()
     # Any code you write here will run before the form opens.
-    user_profile=anvil.server.call(coustmer=user_id)
+    user_profile=anvil.server.call('add_borrower_step1',user_id)
     self.full_name_label.text=user_profile['full_name']
   def button_1_click(self, **event_args):
     open_form('bank_users.borrower_rgistration_form.boorrower_edit_profile')
