57af2644a1f0a57d993b3d9c6671c4069aceb708
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-11-16 10:13:53 +0000

    Created borrower_registration_form.star_1_borrower_registration_form_begin_3.star_1_borrower_registration_form_begin_3b_business and borrower_registration_form.star_1_borrower_registration_form_begin_3.star_1_borrower_registration_form_begin_3b_student; Edited borrower_registration_form.star_1_borrower_registration_form_begin_4, borrower_registration_form.star_1_borrower_registration_form_begin_3.star_1_borrower_registration_form_begin_3d and borrower_registration_form.star_1_borrower_registration_form_begin_3.star_1_borrower_registration_form_begin_3c

---
+++ client_code/borrower_registration_form/star_1_borrower_registration_form_begin_3/star_1_borrower_registration_form_begin_3b_student/__init__.py
@@ -0,0 +1,34 @@
+from ._anvil_designer import star_1_borrower_registration_form_begin_3b_studentTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class star_1_borrower_registration_form_begin_3b_student(star_1_borrower_registration_form_begin_3b_studentTemplate):
+  def __init__(self, **properties):
+    self.user_id=user_id
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    
+    # Any code you write here will run before the form opens.
+
+  def button_1_copy_1_copy_1_copy_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('borrower_registration_form.star_1_borrower_registration_form_begin_3.star_1_borrower_registration_form_begin_3c',use_id=user_id)
+
+  def home_borrower_registration_form_copy_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('bank_users.user_form')
+
+  def button_1_next_copy_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    college_name=self.borrower_college_name_text.text
+    college_id=self.borrower_college_id_text.text
+    college_proof=self.borrower_college_proof_img.file
+    college_address=self.borrower_college_address_text
+    user_id=self.user_id
+    open_form('borrower_registration_form.star_1_borrower_registration_form_begin_3.star_1_borrower_registration_form_begin_4',user_id=self.userId)
