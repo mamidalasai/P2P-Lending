1253847b18408a89c5f03e1ded56ef7a2b2ebc8c
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2023-11-20 08:31:56 +0000

    Edited bank_users.borrower_rgistration_form.borrower_foreclosure_request.borrower_foreclosure

--- client_code/bank_users/borrower_rgistration_form/borrower_foreclosure_request/borrower_foreclosure/__init__.py
+++ client_code/bank_users/borrower_rgistration_form/borrower_foreclosure_request/borrower_foreclosure/__init__.py
@@ -14,3 +14,11 @@ class borrower_foreclosure(borrower_foreclosureTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('bank_users.borrower_rgistration_form.borrower_foreclosure_request')
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('bank_users.borrower_rgistration_form')
