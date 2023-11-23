5f1a4afcc5aab1818ea5af6d652a6edb3adb4c02
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-23 04:41:12 +0000

    Edited admin.dashboard.manage_products, admin.dashboard.manage_products.edit_form, admin.dashboard.manage_products.manage_producs1, admin.dashboard.manage_products.update_form and 2 more

--- client_code/admin/dashboard/manage_products/__init__.py
+++ client_code/admin/dashboard/manage_products/__init__.py
@@ -15,3 +15,19 @@ class manage_products(manage_productsTemplate):
 
     # Any code you write here will run before the form opens.
 
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.dashboard.manage_products.manage_producs1')
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.dashboard.manage_products.view_product')
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.dashboard.manage_products.view_categories')
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.dashboard')
+
