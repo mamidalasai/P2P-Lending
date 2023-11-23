54dc8d7a76b6f3dc0e84f77e4ce0ca0259db936f
saimamidala333@gmail.com <saimamidala333@gmail.com>
2023-11-23 12:00:03 +0000

    Created admin.dashboard.lenders.view_profile_copy, admin.dashboard.lenders.view_profile_copy.edit_form_copy, admin.dashboard.lenders.view_profile_copy.update_form_copy, admin.dashboard.loan_management.approved_loans and 3 more; Edited admin.dashboard.lenders, admin.dashboard.lenders.RowTemplate4, admin.dashboard.loan_management, admin.dashboard.loan_management.closed_loans and 1 more; Deleted admin.dashboard.admin_view_profile, admin.dashboard.admin_view_profile.RowTemplate2, admin.dashboard.admin_view_profile.admin_view_profile_3, admin.dashboard.admin_view_profile.admin_view_profile_4 and 9 more

--- client_code/admin/dashboard/lenders/RowTemplate4/__init__.py
+++ client_code/admin/dashboard/lenders/RowTemplate4/__init__.py
@@ -14,3 +14,8 @@ class RowTemplate4(RowTemplate4Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    value_to_pass = self.link_1.text
+    open_form('admin.dashboard.lenders.view_profile_copy', value_to_pass)
--- client_code/admin/dashboard/loan_management/approved_loans/RowTemplate2/__init__.py
+++ client_code/admin/dashboard/loan_management/approved_loans/RowTemplate2/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import npa_formTemplate
+from ._anvil_designer import RowTemplate2Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,7 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class npa_form(npa_formTemplate):
+class RowTemplate2(RowTemplate2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -17,4 +17,5 @@ class npa_form(npa_formTemplate):
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
-    open_form('admin.dashboard.loan_management')
+    value_to_pass = self.label_2.text
+    open_form('admin.dashboard.borrowers.view_profile', value_to_pass)
