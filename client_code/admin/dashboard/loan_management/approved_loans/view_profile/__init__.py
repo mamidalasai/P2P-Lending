1363b3ae85c70e9ab783c167ca5c0e17eb85052b
saimamidala333@gmail.com <saimamidala333@gmail.com>
2023-11-23 12:01:31 +0000

    Created admin.dashboard.lenders.view_profile_copy, admin.dashboard.lenders.view_profile_copy.edit_form_copy, admin.dashboard.lenders.view_profile_copy.update_form_copy, admin.dashboard.loan_management.approved_loans and 3 more; Edited admin.dashboard.lenders, admin.dashboard.lenders.RowTemplate4, admin.dashboard.loan_management, admin.dashboard.loan_management.closed_loans and 1 more; Deleted admin.dashboard.admin_view_profile, admin.dashboard.admin_view_profile.RowTemplate2, admin.dashboard.admin_view_profile.admin_view_profile_3, admin.dashboard.admin_view_profile.admin_view_profile_4 and 9 more

--- client_code/admin/dashboard/loan_management/approved_loans/view_profile/__init__.py
+++ client_code/admin/dashboard/loan_management/approved_loans/view_profile/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import manual_disbursementTemplate
+from ._anvil_designer import view_profileTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,13 +8,10 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class manual_disbursement(manual_disbursementTemplate):
+class view_profile(view_profileTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('admin.dashboard.loan_management')
