d0b2298bd1d4d1f3851c087d5391306b14fb952a
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-11-23 04:23:22 +0000

    Edited lendor_registration_form.Lender_reg_form_2, lendo_server and lendor_registration_form.Lender_reg_form_1

--- client_code/lendor_registration_form/Lender_reg_form_2/__init__.py
+++ client_code/lendor_registration_form/Lender_reg_form_2/__init__.py
@@ -1,3 +1,4 @@
+
 from ._anvil_designer import Lender_reg_form_2Template
 from anvil import *
 import anvil.server
@@ -13,11 +14,32 @@ class Lender_reg_form_2(Lender_reg_form_2Template):
     self.userId = user_id
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+   
+    user_data = anvil.server.call('get_user_data', user_id)
+        
+    if user_data:
+            self.mobile = user_data.get('mobile', '')
+            self.another_email = user_data.get('another_email', '')
+            
+            
+    else:
+        self.mobile = ''
+        self.another_email = ''
+        
+
+       #Restore previously entered data if available
+    if self.mobile:
+            self.text_box_1.text = self.mobile
+    if self.another_email:
+            self.text_box_2.text = self.another_email
+    
+            
+        
 
     # Any code you write here will run before the form opens.
 
   
-
+    
   def button_1_click(self, **event_args):
     user_id = self.userId
     open_form('lendor_registration_form.Lender_reg_form_1',user_id=user_id)
@@ -38,36 +60,11 @@ class Lender_reg_form_2(Lender_reg_form_2Template):
       Notification("Please fill all the fields").show()
     else:
      anvil.server.call('add_lendor_second_form', mobile, email, photo, user_id)
-     open_form('lendor_registration_form.Lender_reg_form_3e', user_id=user_id)
+     open_form('lendor_registration_form.Lender_reg_form_3', user_id=user_id)
 
 
-  
-    #if not mobile or not email or not photo:
-      #Notification("Please fill all the fields").show()
-    #if len(mobile) != 10:
-      #Notification("Enter a vaild Mobile number").show()
-    #else:
-      #anvil.server.call('add_lendor_second_form',mobile,email,photo,user_id)
-      #open_form('lendor_registration_form.Lender_reg_form_3',user_id = user_id)
-      
-    
-      
-     #anvil.server.call('add_lendor_second_form',mobile,email,photo,user_id)
-     #open_form('lendor_registration_form.Lender_reg_form_3',user_id = user_id)
- 
-      
-      
-      
-    
-  
-    #if not mobile or not email or not photo :
-     #Notification("Please fill all the fields").show()
-    #else:
-     
 
   
   def button_3_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form("bank_users.user_form")
-    
-    
