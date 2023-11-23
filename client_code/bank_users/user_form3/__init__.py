85882086bd6af0c7c5d6d4e9c61f84487d1b98c7
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2023-11-23 04:13:29 +0000

    Created bank_users.user_form3.contact_user_form, bank_users.user_form3, bank_users.user_form3.about_user_form and bank_users.user_form.user_form_copy; Edited bank_users.user_form; Deleted bank_users.user_form1, bank_users.user_form.about_user_form and bank_users.user_form.contact_user_form

--- client_code/bank_users/user_form3/__init__.py
+++ client_code/bank_users/user_form3/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import user_formTemplate
+from ._anvil_designer import user_form3Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -7,13 +7,11 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..user_form import user_module
-from ..borrower_rgistration_form import borrower_main_form_module
+from . import user_module
 from ..main_form import main_form_module
 
-class user_form(user_formTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+class user_form3(user_form3Template):
+  def __init__(self,**properties):
     self.init_components(**properties)
     self.email = main_form_module.email
     email=self.email
@@ -21,31 +19,37 @@ class user_form(user_formTemplate):
     self.user_id =  user_module.find_user_id(email)
     
     self.email = email
+    self.user_name_lable.text = self.name
     if main_form_module.alert_mes(main_form_module.flag):
       print("user login")
     else:
       print("user login")
 
-    # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-     alert("Logged out sucessfully")
+  
+  def logout_user_form_link_click(self, **event_args):
+     alert("logout sucessfully")
      anvil.users.logout()
      open_form('bank_users.main_form')
+    
 
-  def home_main_form_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form("bank_users.user_form1")
-
-  def borrower_button_click(self, **event_args):
+  
+  # this function is use for new borrower signup and check the user already  signup or not
+  def borrower_user_form_link_click(self, **event_args):
     userid = self.user_id
     open_form('borrower_registration_form.star_1_borrower_registration_form_begin',user_id=userid)
+   
 
-  def lendor_button_click(self, **event_args):
+
+  #--this metod is for lendor--#
+
+  def lendor_user_form_link_click(self, **event_args):
     userid = self.user_id
     open_form('lendor_registration_form.Lender_reg_form_1',user_id=userid)
-    
-  
-    
 
+  #--------------------------------------------------------------------#
   
+  def view_profile_user_home_click(self, **event_args):
+    name = self.name
+    email = self.email
+    user_id = self.user_id
+    alert(f"View profile:\nName = {name}\nEmail = {email}\nUser Id = {user_id}")
