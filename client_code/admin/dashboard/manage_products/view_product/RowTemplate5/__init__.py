f5db7f8f261f1ac8180fb1543beb6f6b031e49aa
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-23 08:55:07 +0000

    Edited admin.dashboard.manage_products.view_product.RowTemplate5

--- client_code/admin/dashboard/manage_products/view_product/RowTemplate5/__init__.py
+++ client_code/admin/dashboard/manage_products/view_product/RowTemplate5/__init__.py
@@ -17,4 +17,5 @@ class RowTemplate5(RowTemplate5Template):
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
-    
+     value_to_pass = self.link_1.text
+    open_form('admin.dashboard.borrowers.view_profile', value_to_pass)
