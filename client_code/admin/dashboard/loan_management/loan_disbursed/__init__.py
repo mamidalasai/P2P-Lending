0e111ec87c6d74b268e2618f8ce61378d82196b4
saimamidala333@gmail.com <saimamidala333@gmail.com>
2023-11-28 11:46:43 +0000

    Edited admin.dashboard.loan_management.loan_disbursed

--- client_code/admin/dashboard/loan_management/loan_disbursed/__init__.py
+++ client_code/admin/dashboard/loan_management/loan_disbursed/__init__.py
@@ -42,11 +42,10 @@ class loan_disbursed(loan_disbursedTemplate):
 
     c = -1
     for i in self.account:
+      c += 1
       if i != None:
         b = self.account.count(i)
-        
         if b > 1:
-          c = self.account.index(i)
           self.result1.append(self.id[c])
         print(b)
 
@@ -70,6 +69,39 @@ class loan_disbursed(loan_disbursedTemplate):
     print(self.result)
     print(self.result1)
     print(self.main_result)
+
+    self.data = tables.app_tables.loan_details.search()
+
+    a = -1
+    self.list_1 = []
+    self.list_2 = []
+    self.list_3 = []
+    self.list_4 = []
+    
+    
+    for i in self.data:
+      a+=1
+      self.list_1.append(i['loan_id'])
+      self.list_2.append(i['coustmer_id'])
+      self.list_3.append(i['full_name'])
+      self.list_4.append(i['loan_updated_status'])
+    print(a)
+
+    self.result_3 = []
+    self.index = []
+    if a == -1:
+      alert("No Data Available Here!")
+    else:
+      b = -1
+      for i in self.list_4:
+        b+=1
+        if i == "Approved" or i == 'approved':
+          self.index.append(b)
+          
+      for i in self.index:
+        self.result.append({'loan_id' : self.list_1[i], 'coustmer_id' : self.list_2[i], 'full_name' : self.list_3[i], 'loan_status' : self.list_4[i]})
+
+      self.repeating_panel_1.items = self.result
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
