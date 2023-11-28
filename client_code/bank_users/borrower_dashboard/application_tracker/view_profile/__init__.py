a30253ee62b54e43060a4b52a66d5ba1d879e4cc
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-11-28 06:28:08 +0000

    Edited bank_users.borrower_dashboard.application_tracker.view_profile

--- client_code/bank_users/borrower_dashboard/application_tracker/view_profile/__init__.py
+++ client_code/bank_users/borrower_dashboard/application_tracker/view_profile/__init__.py
@@ -1,9 +1,5 @@
 from ._anvil_designer import view_profileTemplate
 from anvil import *
-import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -11,24 +7,28 @@ from .. import borrower_main_form_module as main_form_module
 
 class view_profile(view_profileTemplate):
     def __init__(self, selected_row, **properties):
-        # self.user_id=main_form_module.userId
-        # self.user_id=1000
         self.init_components(**properties)
-    def button_1_copy_click(self, **event_args):
-      open_form('bank_users.borrower_dashboard')
 
-    def button_1_click(self, **event_args):
-     """This method is called when the button is clicked"""
-     open_form('bank_users.borrower_dashboard.application_tracker')
-
-        # Any code you write here will run before the form opens.
-     self.loan_id_label.text = f"{selected_row['loan_id']}"
-     try:
-       app_tables.user_profile.get(coustmer_id=str)
-            user_request = app_tables.us.get(coustmer_id=str(selected_row['coustmer_id']))
+        # Display loan details
+        self.loan_id_label.text = f"{selected_row['loan_id']}"
+        self.loan_amount_label.text=f"{selected_row['total_repayment_amount']}"
+        
+        # Try to get user profile details
+        try:
+            user_request = app_tables.user_profile.get(coustmer_id=selected_row['coustmer_id'])
             if user_request is not None:
                 # Assuming 'bank_acc_details' is a valid column name in the 'borrower' table
-                bank_acc_details = user_request['bank_acc_details']
-                borrower_approve_date_time = user_request['borrower_approve_date_time']
-                self.label_member_since.text = f"{borrower_approve_date_time}"
-                self.label_bank_acc_details.text = f"{bank_acc_details}"
+                full_name = user_request['full_name']
+                email_id=user_request['email_user']
+               # borrower_approve_date_time = user_request['borrower_approve_date_time']
+                self.full_name_label.text = f"{full_name}"
+                
+        except Exception as e:
+            print(f"Error fetching user profile details: {e}")
+
+    def button_1_copy_click(self, **event_args):
+        open_form('bank_users.borrower_dashboard')
+
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        open_form('bank_users.borrower_dashboard.application_tracker')
