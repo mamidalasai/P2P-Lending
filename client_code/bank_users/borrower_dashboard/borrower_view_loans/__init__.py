dcab7948ad81a1e9675863c1d7ea9de015ca5fb6
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-11-30 11:06:10 +0000

    Edited bank_users.borrower_dashboard.borrower_view_loans

--- client_code/bank_users/borrower_dashboard/borrower_view_loans/__init__.py
+++ client_code/bank_users/borrower_dashboard/borrower_view_loans/__init__.py
@@ -7,7 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from .. import borrower_main_form_module as main_form_module
 class borrower_view_loans(borrower_view_loansTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -21,6 +21,7 @@ class borrower_view_loans(borrower_view_loansTemplate):
     self.label_6.text=str(len(self.repeating_panel_2.items))
     self.label_7.text=str(len(self.repeating_panel_3.items))
     self.label_8.text=str(len(self.repeating_panel_4.items))
+    self.label_10.
     # Any code you write here will run before the form opens.
 
   def home_borrower_registration_form_copy_1_click(self, **event_args):
@@ -33,10 +34,12 @@ class borrower_view_loans(borrower_view_loansTemplate):
     self.label_2.visible=False
     self.label_3.visible=False
     self.label_4.visible=False
+    self.label_10.visible=False
     self.data_grid_1.visible=True
     self.data_grid_2.visible=False
     self.data_grid_3.visible=False
     self.data_grid_4.visible=False
+    self.data_grid_5.visible=False
     
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -44,10 +47,12 @@ class borrower_view_loans(borrower_view_loansTemplate):
     self.label_2.visible=True
     self.label_3.visible=False
     self.label_4.visible=False
+    self.label_10.visible=False
     self.data_grid_1.visible=False
     self.data_grid_2.visible=True
     self.data_grid_3.visible=False
     self.data_grid_4.visible=False
+    self.data_grid_5.visible=False
 
   def button_3_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -55,10 +60,12 @@ class borrower_view_loans(borrower_view_loansTemplate):
     self.label_2.visible=False
     self.label_3.visible=True
     self.label_4.visible=False
+    self.label_10.visible=False
     self.data_grid_1.visible=False
     self.data_grid_2.visible=False
     self.data_grid_3.visible=True
     self.data_grid_4.visible=False
+    self.data_grid_5.visible=False
 
   def button_4_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -66,7 +73,22 @@ class borrower_view_loans(borrower_view_loansTemplate):
     self.label_2.visible=False
     self.label_3.visible=False
     self.label_4.visible=True
+    self.label_10.visible=False
     self.data_grid_1.visible=False
     self.data_grid_2.visible=False
     self.data_grid_3.visible=False
     self.data_grid_4.visible=True
+    self.data_grid_5.visible=False
+
+  def button_5_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.label_1.visible=False
+    self.label_2.visible=False
+    self.label_3.visible=False
+    self.label_4.visible=False
+    self.label_10.visible=True
+    self.data_grid_1.visible=False
+    self.data_grid_2.visible=False
+    self.data_grid_3.visible=False
+    self.data_grid_4.visible=False
+    self.data_grid_5.visible=True
