614149378552d4f331f445facd38980937b439f4
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2023-11-30 08:37:40 +0000

    Edited borrower_server, bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure.foreclose and bank_users.borrower_dashboard.borrower_foreclosure_request.borrower_foreclosure

--- server_code/borrower_server.py
+++ server_code/borrower_server.py
@@ -144,41 +144,12 @@ def add_borrower_step9(ifsc,salary_type,select_bank,net_bank, user_id):
 
 
 @anvil.server.callable
-def calculate_credit_limit(user_id):
-    loan_details_table = app_tables.loan_details  # Replace with your actual table name
-
-    # Get all rows for the given user_id
-    user_rows = loan_details_table.search(coustmer_id=user_id)
-
-    if len(user_rows) == 0 or 'credit_limit' not in user_rows[0]:
-        # If there is no recent row or no credit limit in the recent row, return the default credit limit
-        return 200000
-    else:
-        # Sort the rows by timestamp in descending order
-        sorted_rows = sorted(user_rows, key=lambda x: x['timestamp'], reverse=True)
-
-        # Find the most recent row with a valid credit limit
-        most_recent_row = next((row for row in sorted_rows if 'credit_limit' in row), None)
-
-        if most_recent_row:
-            # Calculate the available credit limit based on the most recent row's credit limit and loan amount
-            credit_limit = most_recent_row['credit_limit']
-            loan_amount = most_recent_row['loan_amount']
-            available_credit = credit_limit - loan_amount
-
-            return available_credit
-        else:
-            # If there is no row with a credit limit, return the default credit limit
-            return 200000
-
-# Server function to add loan details
-@anvil.server.callable
 def add_loan_details(loan_amount, credit_limit, tenure, user_id):
     # Generate a unique loan ID and get the updated counter
     loan_id = generate_loan_id()
 
     # Search for the user profile
-    user_profiles = app_tables.UserProfile.search(coustmer_id=user_id)
+    user_profiles = app_tables.user_profile.search(coustmer_id=user_id)
     
     if user_profiles and len(user_profiles) > 0:
         # If there is a user profile, get the first one
@@ -186,15 +157,16 @@ def add_loan_details(loan_amount, credit_limit, tenure, user_id):
 
         # Extract the full name from the user profile
         full_name = user_profile['full_name']
-
+        email_id = user_profile['email_user']
         # Add the loan details to the data table
-        app_tables.LoanDetails.add_row(
+        app_tables.loan_details.add_row(
             loan_id=loan_id,
             loan_amount=loan_amount,
             credit_limit=credit_limit,
             tenure=tenure,
             coustmer_id=user_id,
-            full_name=full_name,  # Store the full name in the loan_details table
+            full_name=full_name,
+            email_id=email_id,                 
             timestamp=datetime.now()
         )
 
@@ -204,10 +176,9 @@ def add_loan_details(loan_amount, credit_limit, tenure, user_id):
         # Handle the case where no user profile is found
         return "User profile not found"
 
-# Helper function to generate a unique loan ID
 def generate_loan_id():
     # Query the latest loan ID from the data table
-    latest_loan = app_tables.LoanDetails.search(tables.order_by("loan_id", ascending=False))
+    latest_loan = app_tables.loan_details.search(tables.order_by("loan_id", ascending=False))
 
     if latest_loan and len(latest_loan) > 0:
         # If there are existing loans, increment the last loan ID
@@ -219,4 +190,23 @@ def generate_loan_id():
 
     # Return the new loan ID
     return f"LA{counter}"
+
+
+
+
+
+
+
+@anvil.server.callable
+def update_loan_details(loan_id, emi, total_repayment_amount, interest_rate):
+    rows = app_tables.loan_details.search(loan_id=loan_id)
+
+    if rows:
+        row = rows[0]
+        row['emi'] = emi
+        row['total_repayment_amount'] = total_repayment_amount
+        row['interest_rate'] = interest_rate
+        row.update()
+    else:
+        raise ValueError(f"Row not found for loan_id {loan_id}")
 
