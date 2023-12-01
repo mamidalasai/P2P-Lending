6d3beb7e72199602fb6c78887eb40c3612fe9e9e
manikantagangisetty537@gmail.com <manikantagangisetty537@gmail.com>
2023-12-01 07:47:42 +0000

    Edited bank_users.borrower_dashboard.borrower_today_dues

--- client_code/bank_users/borrower_dashboard/borrower_today_dues/__init__.py
+++ client_code/bank_users/borrower_dashboard/borrower_today_dues/__init__.py
@@ -7,7 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from .. import borrower_main_form_module as main_form_module 
 
 class borrower_today_dues(borrower_today_duesTemplate):
@@ -20,11 +20,17 @@ class borrower_today_dues(borrower_today_duesTemplate):
         all_loans = app_tables.loan_details.search()
         
         # Calculate days left and days gone for each loan
-        for loans in all_loans:
-          due_date=loans['due_date']
-          days_left=(due_date-datetime.now()).days()
-          loans['days_left']=days_left
-          self.repeating_panel_1.items=all_loans
+        for loan in all_loans:
+            due_date = loan['due_date']
+            days_left = (due_date - datetime.now()).days
+            days_gone = (datetime.now() - loan['timestamp']).days
+            
+            # Add calculated values to the loan row
+            loan['days_left'] = days_left
+            loan['days_gone'] = days_gone
+        
+        # Display loans with the calculated values in the repeating panel
+        self.repeating_panel_1.items = all_loans
 
     def home_borrower_registration_form_copy_1_click(self, **event_args):
         """This method is called when the button is clicked"""
