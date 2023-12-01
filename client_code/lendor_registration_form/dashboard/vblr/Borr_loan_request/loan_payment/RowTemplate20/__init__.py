96266aeb83f1a2c51152c4856829fc7fe1cfe4d9
maniamarthi@gmail.com <maniamarthi@gmail.com>
2023-12-01 07:45:42 +0000

    Created lendor_registration_form.dashboard.vblr.Borr_loan_request.loan_payment.RowTemplate20; Edited settings and lendor_registration_form.dashboard.vblr.Borr_loan_request.loan_payment

--- client_code/lendor_registration_form/dashboard/vblr/Borr_loan_request/loan_payment/RowTemplate20/__init__.py
+++ client_code/lendor_registration_form/dashboard/vblr/Borr_loan_request/loan_payment/RowTemplate20/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import loan_paymentTemplate
+from ._anvil_designer import RowTemplate20Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,13 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class loan_payment(loan_paymentTemplate):
+class RowTemplate20(RowTemplate20Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
